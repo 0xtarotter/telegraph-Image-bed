865498d8ecec39f61889c243f77efeb9606f1c15
TΛRØTTΞR <399929@gmail.com>
2024-11-20 21:53:43 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,21 @@ export default function Footer() {
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
-        <a href="https://39mi.com" target="_blank" style="text-decoration:none"><img src="https://park.39mi.com/img/by.svg" alt="Logo" height="auto" width="150"></a>
+       
+<Link href="https://39mi.com" passHref>
+      <a target="_blank" style={{ textDecoration: 'none' }}>
+        <Image
+          src="https://park.39mi.com/img/by.svg"
+          alt="Logo"
+          width={150}
+          height={auto} // Next.js的Image组件不会直接支持auto，通常你可以使用固定宽度或者高宽比。
+          layout="intrinsic" // 可选，适应图片的原始宽高比
+        />
+      </a>
+    </Link>
+
+
+        
       </div>
     </footer>
   );
