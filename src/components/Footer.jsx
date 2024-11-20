0027fb7376ae3cdf23dfdb0a9f408e55ee16c2c9
TΛRØTTΞR <399929@gmail.com>
2024-11-20 21:03:33 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="">
+    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
+        
         <p className="text-xs text-gray-500">
            
-
-        <a href="https://ip.im"><img src="https://ip.im/img" alt="My IP Address" width="215" height="70" /></a>
-        <a href="https://39mi.com" target="_blank" style={{ textDecoration: 'none', display: 'inline-flex', alignItems: 'center', fontSize: '10px', color: '#dedede' }}> 
-  <img src="https://park.39mi.com/img/domainking.svg" style={{ marginRight: '10px' }} /> 
-  <img src="https://park.39mi.com/img/tx.svg" height="auto" width="150" style={{ marginRight: '10px' }} /> 
-  owner 
+          <a href="https://39mi.com" target="_blank" style="text-decoration:none; display: inline-flex; align-items: center;"> 
+  <img src="https://park.39mi.com/img/domainking.svg" style="margin-right: 10px;" /> 
+  <img src="https://park.39mi.com/img/tx.svg" height="auto" width="150" /> 
+  <span style="font-size:10px;color:#dedede; margin-left: 10px;">owner</span> 
 </a>
+
+
 
 
 
