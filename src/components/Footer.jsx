5f36ffd131359205122d0cc8e8f60e95f5a360c6
TΛRØTTΞR <399929@gmail.com>
2024-11-20 20:54:24 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,12 @@ export default function Footer() {
            
 
         <a href="https://ip.im"><img src="https://ip.im/img" alt="My IP Address" width="215" height="70" /></a>
-         <a href="https://39mi.com" target="_blank" style="text-decoration:none;">
+        <p> <a href="https://39mi.com" target="_blank" style="text-decoration:none;">
           <img src="https://park.39mi.com/img/tx.svg">
-            <br> 
-            <img src="https://park.39mi.com/img/domainking.svg" height="auto" width="150"> <br> 
-            <a style="font-size:10px;color:#dedede;">owner</a> 
-            </a>
-         
+          
+            <img src="https://park.39mi.com/img/domainking.svg" height="auto" width="150">
+            </a></p>
+         <p><a style="font-size:10px;color:#dedede;">owner</a> </p>
 
           
         </p>
