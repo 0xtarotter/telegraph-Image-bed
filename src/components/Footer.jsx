81c17987b570549f70b43b82bb4a57366a537d8d
TΛRØTTΞR <399929@gmail.com>
2024-11-21 03:08:16 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
+    <footer className="w-full  h-1/12 text-center  flex flex-col justify-center items-center">
       <div >
         <p className="text-xs text-gray-500">
           <img src="https://park.39mi.com/img/by.svg" />
