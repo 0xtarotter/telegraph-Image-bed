45b6d5ecbde7f5e45dad53511edc09e7237a2538
TΛRØTTΞR <399929@gmail.com>
2024-11-20 20:47:03 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
+    <footer className="">
       <div >
         <p className="text-xs text-gray-500">
            
 
         <a href="https://ip.im"><img src="https://ip.im/img" alt="My IP Address" width="215" height="70" /></a>
-       
+         <a href="https://39mi.com" target="_blank" style="text-decoration:none;">
+          <img src="https://park.39mi.com/img/tx.svg">
+            <br> 
+            <img src="https://park.39mi.com/img/domainking.svg" height="auto" width="150"> <br> </a>
+         <a style="font-size:10px;color:#dedede;">owner</a> 
+
           
         </p>
       </div>
