6adcc268b305b943f15b96bb98174849c3f8edbf
TΛRØTTΞR <399929@gmail.com>
2024-11-20 21:46:47 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,10 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-
-
-    <footer>
-
-         <a href="https://39mi.com" target="_blank" style="text-decoration:none"><img src="https://park.39mi.com/img/by.svg" alt="Logo" height="auto" width="150"></a>
-     
-
+    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
+      <div >
+        <a href="https://39mi.com" target="_blank" style="text-decoration:none"><img src="https://park.39mi.com/img/by.svg" alt="Logo" height="auto" width="150"></a>
+      </div>
     </footer>
   );
 }
