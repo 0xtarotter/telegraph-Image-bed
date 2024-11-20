62f33a63bf21ce7937bb1c63b221dbe3210ca620
TΛRØTTΞR <399929@gmail.com>
2024-11-20 21:20:52 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,23 +3,20 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
-      <div >
+
         
         <p className="text-xs text-gray-500">
            
-          <a href="https://39mi.com" target="_blank" style={{ textDecoration: 'none' }}>
-  <img src="https://park.39mi.com/img/domainking.svg" />
-  <img src="https://park.39mi.com/img/tx.svg" height="auto" width="150" />
-  <span style={{ fontSize: '10px', color: '#dedede' }}>owner</span>
-</a>
-
+        <a href="https://39mi.com" target="_blank" style="text-decoration:none;">
+          <img src="https://park.39mi.com/img/by.png" />
+          </a>
 
 
 
 
           
         </p>
-      </div>
+
     </footer>
   );
 }
